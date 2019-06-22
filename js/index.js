29d340b4146d610f8bd1e8ef1e0408a72da4b639
Sérgio Moura <smouracalmon@gmail.com>
2019-06-22 10:13:43 -0300

    Adicionado atividades de classe no index.js

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// Alterando o background-color da nav
+
+// Alterando a cor padrão da fonte do body
+
+// Aumentar o tamanho da fonte do header e corrigir altura da linha
+
+// Alterando a cor dos links do menu
+
+// Altere a cor do background da section de classe calls
+
+// Altere a cor dos ícones do bloco de chamada para roxo claro.
+
+// Adicione um item 'Contato' ao menu com href='#'
+
+// Remova o primeiro elemento section calls
+
+// Adicionar um novo article na section calls
+
+// Criar um elemento style com regras próprias e adicioná-lo
+// à head da página
+// style deve equivaler ao elemento
+// <style>
+// 		body {
+// 			font-family: 'Poppins',sans-serif;
+// 		}
+// </style>
+
+// Crie um elemento style definindo uma regra para :hover mudar o bgc de um elemento
+// aplique essa regra aos itens do menu
